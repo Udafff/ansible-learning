ed3f4e83532f1bcff04ed5697e4e97ee53242f37
Udovichenko AV <aptem-1986@yandex.ru>
2019-09-04 18:05:58 +0300

    Commit: M  add_user.sh A  docker/flask/Dockerfile A  docker/flask/hello_flask.py A  docker/golang/Read.txt A  docker/golang/hello-world A  docker/golang/hello-world.go A  docker/ping/Dockerfile A  machines/docker-server A  playbooks/docker.yml M  playbooks/inventory A  playbooks/lxd-centos-provision.yml M  playbooks/lxd-install.yml A  playbooks/roles/docker/tasks/install-docker.yml A  playbooks/roles/docker/tasks/main.yml M  playbooks/roles/install-packages/tasks/install_packages.yml M  playbooks/roles/os-user/tasks/main.yml D  playbooks/test_lxd_connection.yml

--- add_user.sh
+++ add_user.sh
@@ -1,9 +1,12 @@
-lxc exec centos-01 -- adduser vagrant -c "Vagrant user" -m -p $6$5n7darR1b$KJBApXriM7D1a.eASwDqf14WtFbf/8VfJ61e1zqsP9F6DBihjNF7kWjh1HZJhGr3iuZuoNLNn.5bbkDj78Awo/ -s /bin/bash
+# P@ssw0rd001
+lxc exec centos-01 -- adduser vagrant -c "Vagrant user" -m -p '$6$AzpvezuLJ6TGSNUL$BPhlJgN0O2Yo4GHYTa18/t7UnP/.C1.yvytLV6LJp0UVkfi5IczN7yFTZE9caYorQPmEx2/tU73YQ/q/Ncl9h0' -s /bin/bash
 lxc exec centos-01 -- mkdir -m=700 /home/vagrant/.ssh
-lxc exec centos-01 -- touch /home/vagrant/.ssh/authorized_keys
+lxc exec centos-01 -- sh -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDh+nJc1QJTLtI8KWfiIy+1GBmClFj4wQK0GKtEw8ACqaOxLUjJYk3LVvgoYTtQhNMV3GN2Rh8Ql9dbHbOA8JNkjqM/DkxVLw/6gArhYC1Wa8b6lZ9plU4REMgHJkVf3ooxtjiPVd9GHRjy8YkRC2ICIh5mGwQvi1fSR/qA5Xd9nu8VWKUW88vLxg0stenUw5Y6N5d6kf03BD9L9g0VyjY0t6qCNaqv3h7Y/iZx2+s8QHTHOdqqnpmG2EBauAuJXBp1M6ooyccSzx4Lz6NCuqw/+BJLLXfHmPiUwNv5IVswgM6nMrhmSUMCVTNbCAjDawWCYJfMLWEWj4akowFV2355' >> /home/vagrant/.ssh/authorized_keys"
 lxc exec centos-01 -- chmod 600 /home/vagrant/.ssh/authorized_keys
 lxc exec centos-01 -- chown -R vagrant:vagrant /home/vagrant/.ssh
-lxc exec centos-01 -- echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDh+nJc1QJTLtI8KWfiIy+1GBmClFj4wQK0GKtEw8ACqaOxLUjJYk3LVvgoYTtQhNMV3GN2Rh8Ql9dbHbOA8JNkjqM/DkxVLw/6gArhYC1Wa8b6lZ9plU4REMgHJkVf3ooxtjiPVd9GHRjy8YkRC2ICIh5mGwQvi1fSR/qA5Xd9nu8VWKUW88vLxg0stenUw5Y6N5d6kf03BD9L9g0VyjY0t6qCNaqv3h7Y/iZx2+s8QHTHOdqqnpmG2EBauAuJXBp1M6ooyccSzx4Lz6NCuqw/+BJLLXfHmPiUwNv5IVswgM6nMrhmSUMCVTNbCAjDawWCYJfMLWEWj4akowFV2355' >> /home/vagrant/.ssh/authorized_keys
 lxc exec centos-01 -- yum install openssh-server -y
 lxc exec centos-01 -- systemctl start sshd
 lxc exec centos-01 -- systemctl enable sshd
+lxc exec centos-01 -- usermod -p '$6$AzpvezuLJ6TGSNUL$BPhlJgN0O2Yo4GHYTa18/t7UnP/.C1.yvytLV6LJp0UVkfi5IczN7yFTZE9caYorQPmEx2/tU73YQ/q/Ncl9h0' root
+lxc exec centos-01 -- yum install less -y
+lxc exec centos-01 -- yum install epel-release -y
